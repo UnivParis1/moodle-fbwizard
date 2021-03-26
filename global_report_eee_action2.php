1a1e1236a295af4f63af3ee3d63c73e929bfd139
Isham Yagoub <isham.yagoub@univ-paris1.fr>
2021-03-26 10:28:46 +0100

    Add rtrim to multichoice

--- global_report_eee_action2.php
+++ global_report_eee_action2.php
@@ -143,7 +143,7 @@ if (is_siteadmin()) {
 
 							foreach ($ans_array as $key => $value) {
 								$reponse.= $key > 0 ? " / " : "";
-								$reponse .= $pres_array[intval($value) - 1] ."</br>";
+								$reponse .= rtrim($pres_array[intval($value) - 1]);
 							}
 								
 							$array_csv[$cptl][$p]= rtrim(html_entity_decode(strip_tags( $reponse)));
